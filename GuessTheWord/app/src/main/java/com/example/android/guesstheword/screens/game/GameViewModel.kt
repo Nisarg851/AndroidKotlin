96025ac01f66f54b68c9761af6ce95954d53bb10
Nisarg851 <nisargmahyavanshi8501@gmail.com>
2022-02-07 22:09:25 +0530

    LiveData functionalities

--- GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,15 +1,72 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel(){
-    init{
-        Log.i("ViewModel", "Game ViewModel Created!")
+    private var _word = MutableLiveData<String>();
+    val word: LiveData<String>
+        get() = _word;
+
+    private var _score = MutableLiveData<Int>();
+    val score: LiveData<Int>
+        get() = _score;
+
+    private lateinit var wordList: MutableList<String>
+
+    private fun resetList() {
+        wordList = mutableListOf(
+            "queen",
+            "hospital",
+            "basketball",
+            "cat",
+            "change",
+            "snail",
+            "soup",
+            "calendar",
+            "sad",
+            "desk",
+            "guitar",
+            "home",
+            "railway",
+            "zebra",
+            "jelly",
+            "car",
+            "crow",
+            "trade",
+            "bag",
+            "roll",
+            "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    init {
+        _word.value = "";
+        _score.value = 0;
+        resetList()
+        nextWord()
+        Log.i("GameViewModel", "GameViewModel created!")
+    }
+    private fun nextWord() {
+        if (!wordList.isEmpty()) {
+            _word.value = wordList.removeAt(0)
+        }
+    }
+    fun onSkip() {
+        _score.value = (score.value)?.minus(1);
+        nextWord()
+    }
+
+    fun onCorrect() {
+        _score.value = (score.value)?.plus(1);
+        nextWord()
     }
 
     override fun onCleared() {
         super.onCleared()
-        Log.i("ViewModel", "onCleared: Game ViewModel destroyed!")
+        Log.i("GameViewModel", "GameViewModel destroyed!")
     }
 }
