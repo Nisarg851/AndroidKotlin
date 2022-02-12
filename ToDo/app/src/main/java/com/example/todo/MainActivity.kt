5a036760f4ebcb9568fe8f71a81536d4523e56ac
Nisarg851 <nisargmahyavanshi8501@gmail.com>
2022-02-12 20:00:40 +0530

    DataBinding Applied

--- ToDo/app/src/main/java/com/example/todo/MainActivity.kt
+++ ToDo/app/src/main/java/com/example/todo/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.todo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todo.databinding.ActivityMainBinding
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var taskList: RecyclerView
+    private lateinit var taskInputLayout: TextInputLayout
+    private lateinit var taskInput: TextInputEditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // Recycler View
+        taskList = binding.taskList
+
+        taskInputLayout = binding.taskInputLayout
+        taskInput = binding.taskInput
     }
 }
