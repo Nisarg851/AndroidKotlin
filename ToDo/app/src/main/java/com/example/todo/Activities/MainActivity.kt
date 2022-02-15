5399779c805834620c936951b705270883782b83
Nisarg851 <nisargmahyavanshi8501@gmail.com>
2022-02-15 11:42:39 +0530

    Refresh Recycler View Inefficient Method

--- ToDo/app/src/main/java/com/example/todo/Activities/MainActivity.kt
+++ ToDo/app/src/main/java/com/example/todo/Activities/MainActivity.kt
@@ -2,33 +2,62 @@ package com.example.todo.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.util.Log
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todo.R
+import com.example.todo.RecyclerAdapters.TaskListRecyclerAdapter
 import com.example.todo.databinding.ActivityMainBinding
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    // binding variable
     private lateinit var binding: ActivityMainBinding
 
-    private lateinit var taskList: RecyclerView
+    // Layout ViewGroups and Containers
+    private lateinit var rvTaskList: RecyclerView
+
+    // Layout views
     private lateinit var taskInputLayout: TextInputLayout
     private lateinit var taskInput: TextInputEditText
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    // Other Variables
+    val tasks = ArrayList<String>()
 
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        // initiating binding variable
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        // Recycler View
-        var tasks: List<String> = ArrayList<String>()
-        taskList = binding.rvTaskList
-
+        initiateRecyclerView()
 
         taskInputLayout = binding.taskInputLayout
         taskInput = binding.etTaskInput
+
+        taskInputLayout.setEndIconOnClickListener(View.OnClickListener {
+            val task: Editable? = taskInput.text
+            tasks.add(task.toString())
+            // refreshes whole layout "Not Efficient"
+            rvTaskList.requestLayout()
+        })
+    }
+
+    private fun initiateRecyclerView(){
+        // Recycler View's sample data
+        tasks.add("Task1")
+        tasks.add("Task2")
+        tasks.add("Task3")
+
+        rvTaskList = binding.rvTaskList
+        rvTaskList.adapter = TaskListRecyclerAdapter(tasks)
+        Log.i("RecyclerView", "onCreate: Adapter bounded")
+    }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
     }
 }
--- ToDo/app/src/main/java/com/example/todo/RecyclerAdapters/TaskListRecyclerAdapter.kt
+++ ToDo/app/src/main/java/com/example/todo/RecyclerAdapters/TaskListRecyclerAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.todo.RecyclerAdapters
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,16 +15,19 @@ class TaskListRecyclerAdapter(taskList: List<String>): RecyclerView.Adapter<Task
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){}
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        Log.i("RecyclerView", "onCreateViewHolder: started")
         val view = LayoutInflater.from(parent.context).inflate(R.layout.task_list_item_layout, parent, false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        Log.i("RecyclerView", "onBindViewHolder: started")
         val task: String = taskList.get(position)
         holder.itemView.findViewById<TextView>(R.id.tvTaskTitle).text = task
     }
 
     override fun getItemCount(): Int {
+        Log.i("RecyclerView", "getItemCount: ${taskList.size}")
         return taskList.size
     }
 }
