9f7dad857173ec959e92d94c56ce3faef8fa4fbf
Nisarg851 <nisargmahyavanshi8501@gmail.com>
2022-02-22 13:19:52 +0530

    ReStart

--- ToDo/app/src/main/java/com/example/todo/RecyclerAdapters/TaskListRecyclerAdapter.kt
+++ ToDo/app/src/main/java/com/example/todo/RecyclerAdapters/TaskListRecyclerAdapter.kt
@@ -16,45 +16,7 @@ class TaskListRecyclerAdapter(context: Context, taskList: List<String>): Recycle
     private var context: Context = context
     private var taskList: List<String> = taskList
 
-    class ViewHolder(context: Context,itemView: View): RecyclerView.ViewHolder(itemView), View.OnClickListener{
-
-        private var context: Context = context
-
-        private var taskLayout: ConstraintLayout
-        private var taskDescription: TextView
-        private var taskDelete: TextView
-
-        private var fullTaskVisible: Int = View.GONE
-
-        init {
-            taskLayout = itemView.findViewById(R.id.clTaskLayout)
-            taskDescription = itemView.findViewById(R.id.tvTaskDescription)
-            taskDelete = itemView.findViewById(R.id.tvTaskDelete)
-
-            taskLayout.setOnClickListener(this)
-            taskDescription.setOnClickListener(this)
-            taskDelete.setOnClickListener(this)
-        }
-
-        override fun onClick(view: View?) {
-            when(view?.id){
-                R.id.clTaskLayout -> {
-                    if(fullTaskVisible == View.VISIBLE){
-                        taskLayout.layoutParams.height = Helper.convertDPToPixel(context,64f)
-                        taskLayout.setBackgroundResource(0)
-                        fullTaskVisible = View.GONE
-                    }else{
-                        taskLayout.layoutParams.height = ConstraintLayout.LayoutParams.WRAP_CONTENT
-                        taskLayout.setBackgroundResource(R.drawable.highlighted_background)
-                        fullTaskVisible = View.VISIBLE
-                    }
-                    taskDescription.visibility = fullTaskVisible
-                    taskDelete.visibility = fullTaskVisible
-                }
-            }
-        }
-
-    }
+    class ViewHolder(context: Context,itemView: View): RecyclerView.ViewHolder(itemView){}
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         Log.i("RecyclerView", "onCreateViewHolder: started")
@@ -65,7 +27,6 @@ class TaskListRecyclerAdapter(context: Context, taskList: List<String>): Recycle
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         Log.i("RecyclerView", "onBindViewHolder: started")
         val task: String = taskList.get(position)
-        holder.itemView.findViewById<TextView>(R.id.tvTaskTitle).text = task
     }
 
     override fun getItemCount(): Int {
